2432e99b52df45e2b34f941470ac38922f18d015
bogdanac <bogdana.carausan@yahoo.com>
2018-06-19 21:00:38 +0300

    added samrtmonitor.ui with a draft metroframework form

---
+++ SmartMonitor.UI/Extensions/MyExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel;
+using MetroFramework;
+using MetroFramework.Components;
+using MetroFramework.Forms;
+
+namespace SmartMonitor.UI.Extensions
+{
+    public static class MyExtensions
+    {
+        private const MetroColorStyle FormStyle = MetroColorStyle.Green;
+
+        public static void SetStyle(this IContainer container, MetroForm ownerForm)
+        {
+            if (container == null)
+            {
+                container = new Container();
+            }
+
+            var manager = new MetroStyleManager(container);
+            manager.Owner = ownerForm;
+            container.SetDefaultStyle(ownerForm, FormStyle);
+        }
+
+        public static void SetDefaultStyle(this IContainer contr, MetroForm owner, MetroColorStyle style)
+        {
+            MetroStyleManager manager = FindManager(contr, owner);
+            manager.Style = style;
+            owner.Style = style;
+        }
+
+        public static void SetDefaultTheme(this IContainer contr, MetroForm owner, MetroThemeStyle theme)
+        {
+            MetroStyleManager manager = FindManager(contr, owner);
+            manager.Theme = theme;
+        }
+
+        private static MetroStyleManager FindManager(IContainer contr, MetroForm owner)
+        {
+            MetroStyleManager manager = null;
+            foreach (IComponent item in contr.Components)
+            {
+                if (((MetroStyleManager)item).Owner == owner)
+                {
+                    manager = (MetroStyleManager)item;
+                }
+            }
+
+            return manager;
+        }
+    }
+}
---
+++ SmartMonitor.UI/Form1.cs
@@ -0,0 +1,17 @@
+﻿using SmartMonitor.UI.Extensions;
+using System.Windows.Forms;
+using MetroFramework;
+
+namespace SmartMonitor.UI
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.components.SetStyle(this);
+            this.components.SetDefaultTheme(this, MetroThemeStyle.Dark);
+
+        }
+    }
+}
